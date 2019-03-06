ca362cb9c131227c264865404f8eb8cd2706ec77
Perry Miller <Perry.Miller@esi-group.com>
2019-03-05 23:07:24 -0500

    Copied run script from usul

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,52 @@
-# https://stackoverflow.com/questions/9772036/pass-all-variables-from-one-shellscript-to-another
-DYLD_FALLBACK_LIBRARY_PATH=".:$OSG_LIB_DIR"
-(
-  export DYLD_FALLBACK_LIBRARY_PATH
-  cd build/bin
-  ./osg_tools_test --abort --use-colour=yes --durations=no
-)
+
+################################################################################
+#
+#  Copyright (c) 2019, Perry L Miller IV
+#  All rights reserved.
+#  MIT License: https://opensource.org/licenses/mit-license.html
+#
+################################################################################
+
+################################################################################
+#
+#  Test script.
+#
+################################################################################
+
+# Exit immediately if one of the following commands do not return zero.
+set -e
+
+# Echo all the commands.
+set -x
+
+rm -rf build
+mkdir build
+cd build
+
+cmake ../ \
+  -DCMAKE_VERBOSE_MAKEFILE=ON \
+  -DCMAKE_BUILD_TYPE="Release" \
+  -DUSUL_ENABLE_CODE_COVERAGE=OFF
+cmake --build .
+
+ls -al ./bin/usul_test
+file ./bin/usul_test
+./bin/usul_test --abort --use-colour=yes --durations=no
+
+cd ../
+rm -rf build
+mkdir build
+cd build
+
+cmake ../ \
+  -DCMAKE_VERBOSE_MAKEFILE=ON \
+  -DCMAKE_BUILD_TYPE="Debug" \
+  -DUSUL_ENABLE_CODE_COVERAGE=ON
+cmake --build .
+
+ls -al ./bin/usul_test
+file ./bin/usul_test
+./bin/usul_test --abort --use-colour=yes --durations=no
+
+cd ../
+gcovr
