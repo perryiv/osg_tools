ab3e7b4f559d7750ed588fb9f5bef394f1af3bb4
Perry Miller <perry@modelspace.com>
2019-12-28 15:52:46 -0500

    Moved namespace to top of file

--- source/OsgTools/Math/Vector4.h
+++ source/OsgTools/Math/Vector4.h
@@ -25,35 +25,33 @@
 #include <cmath>
 
 
+namespace Usul {
+namespace Math {
+
+
 ///////////////////////////////////////////////////////////////////////////////
 //
 //  Adapter functions that see if the vector is finite.
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-namespace Usul
+inline bool isFinite ( const osg::Vec4d &v )
 {
-  namespace Math
-  {
-    inline bool isFinite ( const osg::Vec4d &v )
-    {
-      return (
-        ( true == std::isfinite ( v[0] ) ) &&
-        ( true == std::isfinite ( v[1] ) ) &&
-        ( true == std::isfinite ( v[2] ) ) &&
-        ( true == std::isfinite ( v[3] ) )
-      );
-    }
-    inline bool isFinite ( const osg::Vec4f &v )
-    {
-      return (
-        ( true == std::isfinite ( v[0] ) ) &&
-        ( true == std::isfinite ( v[1] ) ) &&
-        ( true == std::isfinite ( v[2] ) ) &&
-        ( true == std::isfinite ( v[3] ) )
-      );
-    }
-  }
+  return (
+    ( true == std::isfinite ( v[0] ) ) &&
+    ( true == std::isfinite ( v[1] ) ) &&
+    ( true == std::isfinite ( v[2] ) ) &&
+    ( true == std::isfinite ( v[3] ) )
+  );
+}
+inline bool isFinite ( const osg::Vec4f &v )
+{
+  return (
+    ( true == std::isfinite ( v[0] ) ) &&
+    ( true == std::isfinite ( v[1] ) ) &&
+    ( true == std::isfinite ( v[2] ) ) &&
+    ( true == std::isfinite ( v[3] ) )
+  );
 }
 
 
@@ -63,27 +61,21 @@ namespace Usul
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-namespace Usul
+inline osg::Vec4d convert ( const Usul::Math::Vec4d &v )
 {
-  namespace Math
-  {
-    inline osg::Vec4d convert ( const Usul::Math::Vec4d &v )
-    {
-      return osg::Vec4d ( v[0], v[1], v[2], v[3] );
-    }
-    inline osg::Vec4f convert ( const Usul::Math::Vec4f &v )
-    {
-      return osg::Vec4f ( v[0], v[1], v[2], v[3] );
-    }
-    inline Usul::Math::Vec4d convert ( const osg::Vec4d &v )
-    {
-      return Usul::Math::Vec4d ( v[0], v[1], v[2], v[3] );
-    }
-    inline Usul::Math::Vec4f convert ( const osg::Vec4f &v )
-    {
-      return Usul::Math::Vec4f ( v[0], v[1], v[2], v[3] );
-    }
-  }
+  return osg::Vec4d ( v[0], v[1], v[2], v[3] );
+}
+inline osg::Vec4f convert ( const Usul::Math::Vec4f &v )
+{
+  return osg::Vec4f ( v[0], v[1], v[2], v[3] );
+}
+inline Usul::Math::Vec4d convert ( const osg::Vec4d &v )
+{
+  return Usul::Math::Vec4d ( v[0], v[1], v[2], v[3] );
+}
+inline Usul::Math::Vec4f convert ( const osg::Vec4f &v )
+{
+  return Usul::Math::Vec4f ( v[0], v[1], v[2], v[3] );
 }
 
 
@@ -93,26 +85,24 @@ namespace Usul
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-namespace Usul
+inline void clamp ( osg::Vec4d &v, const double mn, const double mx )
 {
-  namespace Math
-  {
-    inline void clamp ( osg::Vec4d &v, const double mn, const double mx )
-    {
-      v[0] = std::clamp ( v[0], mn, mx );
-      v[1] = std::clamp ( v[1], mn, mx );
-      v[2] = std::clamp ( v[2], mn, mx );
-      v[3] = std::clamp ( v[3], mn, mx );
-    }
-    inline void clamp ( osg::Vec4f &v, const float mn, const float mx )
-    {
-      v[0] = std::clamp ( v[0], mn, mx );
-      v[1] = std::clamp ( v[1], mn, mx );
-      v[2] = std::clamp ( v[2], mn, mx );
-      v[3] = std::clamp ( v[3], mn, mx );
-    }
-  }
+  v[0] = std::clamp ( v[0], mn, mx );
+  v[1] = std::clamp ( v[1], mn, mx );
+  v[2] = std::clamp ( v[2], mn, mx );
+  v[3] = std::clamp ( v[3], mn, mx );
 }
+inline void clamp ( osg::Vec4f &v, const float mn, const float mx )
+{
+  v[0] = std::clamp ( v[0], mn, mx );
+  v[1] = std::clamp ( v[1], mn, mx );
+  v[2] = std::clamp ( v[2], mn, mx );
+  v[3] = std::clamp ( v[3], mn, mx );
+}
+
+
+} // namespace Math
+} // namespace Usul
 
 
 #endif // _OSG_TOOLS_VECTOR_4D_MATH_FUNCTIONS_H_
