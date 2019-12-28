2ac04c5af4901c3875d01d079ebb9bffabe06411
Perry Miller <perry@modelspace.com>
2019-12-28 15:56:03 -0500

    Added functions to transform and normalize a sequence of vec3

---
+++ source/OsgTools/Math/Sequence.h
@@ -0,0 +1,166 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2019, Perry L Miller IV
+//  All rights reserved.
+//  MIT License: https://opensource.org/licenses/mit-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Math functions for sequences scalars and vectors.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _OSG_TOOLS_MATH_SEQUENCE_FUNCTIONS_H_
+#define _OSG_TOOLS_MATH_SEQUENCE_FUNCTIONS_H_
+
+#include "OsgTools/Math/Matrix.h"
+#include "OsgTools/Math/Vector3.h"
+
+#include "osg/Matrixd"
+#include "osg/Matrixf"
+#include "osg/Vec3d"
+#include "osg/Vec3f"
+
+#include <vector>
+
+
+namespace Usul {
+namespace Math {
+
+
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Helper function to transform a sequence of vec3 elements.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+namespace Details
+{
+  template < class MatrixType, class Vec3Type >
+  inline void transform ( const MatrixType &m, const std::vector < Vec3Type > &a, std::vector < Vec3Type > &b )
+  {
+    // Shortcuts.
+    typedef std::vector < Vec3Type > Sequence;
+    typedef typename Sequence::size_type Size;
+
+    // Needed below.
+    const Size num = a.size();
+
+    // Resize if we have to.
+    // This also handles the case when a and b are the same vector.
+    if ( b.size() != num )
+    {
+      b.resize ( num );
+    }
+
+    // Loop through the sequence.
+    for ( Size i = 0; i < num; ++i )
+    {
+      Usul::Math::multiply ( m, a[i], b[i] );
+    }
+  }
+  template < class MatrixType, class Vec3Type >
+  inline void transform ( const MatrixType &m, std::vector < Vec3Type > &a )
+  {
+    transform ( m, a, a );
+  }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Transform the sequence of vec3 elements.
+//  Note: a and b can be the same vector.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+inline void transform ( const osg::Matrixd &m, const std::vector < osg::Vec3d > &a, std::vector < osg::Vec3d > &b )
+{
+  Details::transform ( m, a, b );
+}
+inline void transform ( const osg::Matrixd &m, std::vector < osg::Vec3d > &a )
+{
+  Details::transform ( m, a );
+}
+inline void transform ( const osg::Matrixf &m, const std::vector < osg::Vec3f > &a, std::vector < osg::Vec3f > &b )
+{
+  Details::transform ( m, a, b );
+}
+inline void transform ( const osg::Matrixf &m, std::vector < osg::Vec3f > &a )
+{
+  Details::transform ( m, a );
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Helper function to normalize a sequence of vec3 elements.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+namespace Details
+{
+  template < class Vec3Type >
+  inline void normalize ( const std::vector < Vec3Type > &a, std::vector < Vec3Type > &b )
+  {
+    // Shortcuts.
+    typedef std::vector < Vec3Type > Sequence;
+    typedef typename Sequence::size_type Size;
+
+    // Needed below.
+    const Size num = a.size();
+
+    // Resize if we have to.
+    // This also handles the case when a and b are the same vector.
+    if ( b.size() != num )
+    {
+      b.resize ( num );
+    }
+
+    // Loop through the sequence.
+    for ( Size i = 0; i < num; ++i )
+    {
+      Usul::Math::normalize ( a[i], b[i] );
+    }
+  }
+  template < class Vec3Type >
+  inline void normalize ( std::vector < Vec3Type > &a )
+  {
+    Details::normalize ( a, a );
+  }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Normalize the sequence of vec3 elements.
+//  Note: a and b can be the same vector.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+inline void normalize ( const std::vector < osg::Vec3d > &a, std::vector < osg::Vec3d > &b )
+{
+  Details::normalize ( a, b );
+}
+inline void normalize ( std::vector < osg::Vec3d > &a )
+{
+  Details::normalize ( a );
+}
+inline void normalize ( const std::vector < osg::Vec3f > &a, std::vector < osg::Vec3f > &b )
+{
+  Details::normalize ( a, b );
+}
+inline void normalize ( std::vector < osg::Vec3f > &a )
+{
+  Details::normalize ( a );
+}
+
+
+} // namespace Math
+} // namespace Usul
+
+
+#endif // _OSG_TOOLS_MATH_SEQUENCE_FUNCTIONS_H_
---
+++ tests/OsgTools/Math/Sequence.cpp
@@ -0,0 +1,202 @@
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2019, Perry L Miller IV
+//  All rights reserved.
+//  MIT License: https://opensource.org/licenses/mit-license.html
+//
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Test the functions for a sequence.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "OsgTools/Math/Sequence.h"
+
+#include "Usul/Math/Constants.h"
+
+#include "catch2/catch.hpp"
+
+#include <sstream>
+#include <vector>
+
+#define SC static_cast < T >
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Select the correct matrix and vec3 type.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+namespace Details
+{
+  template < class T > struct Selector;
+  template <> struct Selector < double >
+  {
+    typedef osg::Matrixd matrix_type;
+    typedef osg::Vec3d vec3_type;
+  };
+  template <> struct Selector < float >
+  {
+    typedef osg::Matrixf matrix_type;
+    typedef osg::Vec3f vec3_type;
+  };
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  See if the values are equal when converted to strings.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+namespace Details
+{
+  template < class T >
+  inline void isEqualString ( const T &a, const T &b )
+  {
+    std::ostringstream ssa, ssb;
+    ssa << a;
+    ssb << b;
+    const std::string sa = ssa.str();
+    const std::string sb = ssb.str();
+    REQUIRE ( sa == sb );
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Test the math functions.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+TEMPLATE_TEST_CASE ( "Functions for a sequence", "", float, double )
+{
+  typedef TestType T;
+  typedef Details::Selector < T > Selector;
+  typedef typename Selector::matrix_type Matrix44;
+  typedef typename Selector::vec3_type Vec3;
+  typedef std::vector < Vec3 > Sequence;
+
+  SECTION ( "Can transform a sequence of vec3 into a new sequence" )
+  {
+    const Matrix44 m = Usul::Math::rotate ( Matrix44(), Vec3 ( SC ( 1 ), SC ( 0 ), SC ( 0 ) ), SC ( Usul::Math::PI_OVER_2 ) );
+    const Sequence a ( {
+      { Vec3 ( SC ( 0 ), SC ( 1 ), SC ( 2 ) ) },
+      { Vec3 ( SC ( 1 ), SC ( 2 ), SC ( 3 ) ) },
+      { Vec3 ( SC ( 2 ), SC ( 3 ), SC ( 4 ) ) },
+      { Vec3 ( SC ( 3 ), SC ( 4 ), SC ( 5 ) ) }
+    } );
+    Sequence b;
+
+    Usul::Math::transform ( m, a, b );
+
+    Details::isEqualString ( b[0][0], SC (  0 ) );
+    Details::isEqualString ( b[0][1], SC ( -2 ) );
+    Details::isEqualString ( b[0][2], SC (  1 ) );
+
+    Details::isEqualString ( b[1][0], SC (  1 ) );
+    Details::isEqualString ( b[1][1], SC ( -3 ) );
+    Details::isEqualString ( b[1][2], SC (  2 ) );
+
+    Details::isEqualString ( b[2][0], SC (  2 ) );
+    Details::isEqualString ( b[2][1], SC ( -4 ) );
+    Details::isEqualString ( b[2][2], SC (  3 ) );
+
+    Details::isEqualString ( b[3][0], SC (  3 ) );
+    Details::isEqualString ( b[3][1], SC ( -5 ) );
+    Details::isEqualString ( b[3][2], SC (  4 ) );
+  }
+
+  SECTION ( "Can transform a sequence of vec3 in place" )
+  {
+    const Matrix44 m = Usul::Math::rotate ( Matrix44(), Vec3 ( SC ( 1 ), SC ( 0 ), SC ( 0 ) ), SC ( Usul::Math::PI_OVER_2 ) );
+    Sequence b ( {
+      { Vec3 ( SC ( 0 ), SC ( 1 ), SC ( 2 ) ) },
+      { Vec3 ( SC ( 1 ), SC ( 2 ), SC ( 3 ) ) },
+      { Vec3 ( SC ( 2 ), SC ( 3 ), SC ( 4 ) ) },
+      { Vec3 ( SC ( 3 ), SC ( 4 ), SC ( 5 ) ) }
+    } );
+
+    Usul::Math::transform ( m, b );
+
+    Details::isEqualString ( b[0][0], SC (  0 ) );
+    Details::isEqualString ( b[0][1], SC ( -2 ) );
+    Details::isEqualString ( b[0][2], SC (  1 ) );
+
+    Details::isEqualString ( b[1][0], SC (  1 ) );
+    Details::isEqualString ( b[1][1], SC ( -3 ) );
+    Details::isEqualString ( b[1][2], SC (  2 ) );
+
+    Details::isEqualString ( b[2][0], SC (  2 ) );
+    Details::isEqualString ( b[2][1], SC ( -4 ) );
+    Details::isEqualString ( b[2][2], SC (  3 ) );
+
+    Details::isEqualString ( b[3][0], SC (  3 ) );
+    Details::isEqualString ( b[3][1], SC ( -5 ) );
+    Details::isEqualString ( b[3][2], SC (  4 ) );
+  }
+
+  SECTION ( "Can normalize a sequence of vec3 into a new sequence" )
+  {
+    const Sequence a ( {
+      { Vec3 ( SC ( 2 ), SC ( 0 ), SC ( 0 ) ) },
+      { Vec3 ( SC ( 0 ), SC ( 2 ), SC ( 0 ) ) },
+      { Vec3 ( SC ( 0 ), SC ( 0 ), SC ( 2 ) ) },
+      { Vec3 ( SC ( 1 ), SC ( 1 ), SC ( 1 ) ) }
+    } );
+    Sequence b;
+
+    Usul::Math::normalize ( a, b );
+
+    Details::isEqualString ( b[0][0], SC ( 1 ) );
+    Details::isEqualString ( b[0][1], SC ( 0 ) );
+    Details::isEqualString ( b[0][2], SC ( 0 ) );
+
+    Details::isEqualString ( b[1][0], SC ( 0 ) );
+    Details::isEqualString ( b[1][1], SC ( 1 ) );
+    Details::isEqualString ( b[1][2], SC ( 0 ) );
+
+    Details::isEqualString ( b[2][0], SC ( 0 ) );
+    Details::isEqualString ( b[2][1], SC ( 0 ) );
+    Details::isEqualString ( b[2][2], SC ( 1 ) );
+
+    const TestType oneOverSquareRootOfThree = SC ( 1 ) / SC ( std::pow ( SC ( 3 ), SC ( 0.5 ) ) );
+    Details::isEqualString ( b[3][0], oneOverSquareRootOfThree );
+    Details::isEqualString ( b[3][1], oneOverSquareRootOfThree );
+    Details::isEqualString ( b[3][2], oneOverSquareRootOfThree );
+  }
+
+  SECTION ( "Can normalize a sequence of vec3 in place" )
+  {
+    Sequence b ( {
+      { Vec3 ( SC ( 2 ), SC ( 0 ), SC ( 0 ) ) },
+      { Vec3 ( SC ( 0 ), SC ( 2 ), SC ( 0 ) ) },
+      { Vec3 ( SC ( 0 ), SC ( 0 ), SC ( 2 ) ) },
+      { Vec3 ( SC ( 1 ), SC ( 1 ), SC ( 1 ) ) }
+    } );
+
+    Usul::Math::normalize ( b );
+
+    Details::isEqualString ( b[0][0], SC ( 1 ) );
+    Details::isEqualString ( b[0][1], SC ( 0 ) );
+    Details::isEqualString ( b[0][2], SC ( 0 ) );
+
+    Details::isEqualString ( b[1][0], SC ( 0 ) );
+    Details::isEqualString ( b[1][1], SC ( 1 ) );
+    Details::isEqualString ( b[1][2], SC ( 0 ) );
+
+    Details::isEqualString ( b[2][0], SC ( 0 ) );
+    Details::isEqualString ( b[2][1], SC ( 0 ) );
+    Details::isEqualString ( b[2][2], SC ( 1 ) );
+
+    const TestType oneOverSquareRootOfThree = SC ( 1 ) / SC ( std::pow ( SC ( 3 ), SC ( 0.5 ) ) );
+    Details::isEqualString ( b[3][0], oneOverSquareRootOfThree );
+    Details::isEqualString ( b[3][1], oneOverSquareRootOfThree );
+    Details::isEqualString ( b[3][2], oneOverSquareRootOfThree );
+  }
+}
