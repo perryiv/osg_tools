fcb8f32aa40724852d659d8f7bbdb78e1c010b08
Perry Miller <perry@modelspace.com>
2021-05-07 22:17:09 -0400

    Fixed typo in conan file

--- conanfile.py
+++ conanfile.py
@@ -20,7 +20,7 @@ class OsgTools(ConanFile):
     scm = {"type": "git", "url": "auto", "revision": "auto"}
     revision_mode = "scm"
     requires = (
-        "USUL/5.1.0@perryiv/stable",
+        "usul/5.1.0@perryiv/stable",
         "openscenegraph/3.6.5",
     )
 
