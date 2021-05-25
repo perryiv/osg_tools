e1eef77acd128ded03a1bc880c66d1348cad0a77
Perry Miller <perry@modelspace.com>
2021-05-25 01:06:31 -0400

    Incremented minor version

--- tests/OsgTools/Version.cpp
+++ tests/OsgTools/Version.cpp
@@ -29,7 +29,7 @@
 TEST_CASE ( "Version should be correct" )
 {
   REQUIRE ( OsgTools::Version::MAJOR == 3 );
-  REQUIRE ( OsgTools::Version::MINOR == 6 );
+  REQUIRE ( OsgTools::Version::MINOR == 7 );
   REQUIRE ( OsgTools::Version::PATCH == 0 );
-  REQUIRE ( std::string ( OsgTools::Version::STRING ) == std::string ( "3.6.0" ) );
+  REQUIRE ( std::string ( OsgTools::Version::STRING ) == std::string ( "3.7.0" ) );
 }
