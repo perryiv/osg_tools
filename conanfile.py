451ad9f458d4bb73bfad070c817c3fdce97b6db5
Perry Miller <perry@modelspace.com>
2021-07-11 02:04:54 -0400

    Using latest usul

--- conanfile.py
+++ conanfile.py
@@ -20,7 +20,7 @@ class OsgTools(ConanFile):
     scm = {"type": "git", "url": "auto", "revision": "auto"}
     revision_mode = "scm"
     requires = (
-        "usul/6.0.0@perryiv/stable",
+        "usul/6.1.0@perryiv/stable",
         "openscenegraph/3.6.5",
     )
 
