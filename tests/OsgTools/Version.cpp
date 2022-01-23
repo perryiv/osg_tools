706d03c0e7855d03a4078565da06b569017fa728
Perry Miller <perry@modelspace.com>
2022-01-22 20:50:29 -0500

    Updated test

--- tests/OsgTools/Version.cpp
+++ tests/OsgTools/Version.cpp
@@ -28,8 +28,8 @@
 
 TEST_CASE ( "Version should be correct" )
 {
-  REQUIRE ( OsgTools::Version::MAJOR ==  3 );
-  REQUIRE ( OsgTools::Version::MINOR == 10 );
-  REQUIRE ( OsgTools::Version::PATCH ==  0 );
-  REQUIRE ( std::string ( OsgTools::Version::STRING ) == std::string ( "3.10.0" ) );
+  REQUIRE ( OsgTools::Version::MAJOR == 4 );
+  REQUIRE ( OsgTools::Version::MINOR == 0 );
+  REQUIRE ( OsgTools::Version::PATCH == 0 );
+  REQUIRE ( std::string ( OsgTools::Version::STRING ) == std::string ( "4.0.0" ) );
 }
