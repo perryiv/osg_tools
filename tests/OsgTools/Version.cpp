ac56e40997692417f3a4db0da73e58b60c040721
Perry Miller <perry@modelspace.com>
2022-04-02 22:08:29 -0400

    Incremented patch number

--- tests/OsgTools/Version.cpp
+++ tests/OsgTools/Version.cpp
@@ -30,6 +30,6 @@ TEST_CASE ( "Version should be correct" )
 {
   REQUIRE ( OsgTools::Version::MAJOR == 4 );
   REQUIRE ( OsgTools::Version::MINOR == 0 );
-  REQUIRE ( OsgTools::Version::PATCH == 0 );
-  REQUIRE ( std::string ( OsgTools::Version::STRING ) == std::string ( "4.0.0" ) );
+  REQUIRE ( OsgTools::Version::PATCH == 1 );
+  REQUIRE ( std::string ( OsgTools::Version::STRING ) == std::string ( "4.0.1" ) );
 }
